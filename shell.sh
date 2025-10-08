88887f17dbcbf2b3179cb2aba9fb82e2f16032c3
Loknath <devopsloknath94@gmail.com>
2025-10-08 20:46:01 +0530

    s

--- shell.sh
+++ shell.sh
@@ -7,11 +7,31 @@ N="\e[0m"
 echo "this script is started execution at : $TIMESTAMP" &>> $LOGFILE
 TIMESTAMP=$(date -%F)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 .. failed"
+    else
+        echo "$2 .. success"
+    fi
+}
 
-cp /root/Loknath/roboshop-shell-1/mongodb-org.repo /etc/yum.repos.d/mongodb-org.repo
-sudo yum install -y mongodb-org
+cp .mongodb-org.repo /etc/yum.repos.d/mongodb-org.repo
+
+VALIDATE $1 "copying mongo-file"
+
+sudo yum install -y mongodb-org  &>> LOGFILE
+
+VALIDATE $? "installation of mongodb"
+
 systemctl enable mongod
+
+VALIDATE $? "enabling mongodb"
+
 systemctl start mongod
+
+VALIDATE $? "starting mongodb"
+
 sudo systemctl status mongod
 mongo --version
 
